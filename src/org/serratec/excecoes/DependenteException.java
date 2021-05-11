0abc460062d9354ad151bd507836df1e480eb76e
RodrigoBR-dev <rodrigocmartins@hotmail.com>
2021-05-10 22:33:23 -0300

    Id da classe DependenteException

--- src/org/serratec/excecoes/DependenteException.java
+++ src/org/serratec/excecoes/DependenteException.java
@@ -2,6 +2,11 @@ package org.serratec.excecoes;
 
 public class DependenteException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2L;
+
 	public DependenteException(String msg) {
 		super (msg);
 	}	
